abb56bbbd8de7f18012ae7b6da4b00029fe34c0c
HuaGuang <2475096613@qq.com>
2023-09-25 21:34:13 +0800

    修复 ”条形把百分比文本挤出“ 的 Bug。

--- app/src/main/java/com/huaguang/flowoftime/ui/widget/HorizontalBarChart.kt
+++ app/src/main/java/com/huaguang/flowoftime/ui/widget/HorizontalBarChart.kt
@@ -5,12 +5,8 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -19,8 +15,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.huaguang.flowoftime.utils.formatDurationInText
@@ -79,11 +77,15 @@ fun HorizontalBarChart(
 @Composable
 fun HorizontalBarChartPreview() {
     val sampleData = listOf(
-        "Category AAAAAAAA" to 50f,
-        "Category B" to 30f,
-        "Category C" to 70f
+        "核心" to 181f,
+        "框架" to 176f,
+        "常务" to 171f,
+        "休息" to 70f,
+        "xxx" to 41f,
+        "家人" to 31f,
+        "违破" to 24f,
     )
-    HorizontalBarChart(data = sampleData, referenceValue = 70f, maxValue = 150f) {
+    HorizontalBarChart(data = sampleData, referenceValue = 181f, maxValue = 696f) {
 
     }
 }
@@ -97,40 +99,45 @@ fun Bar(
     barColor: Color,
     onClick: () -> Unit
 ) {
-
-//    RDALogger.info("formattedDuration = $formattedDuration, ratio = $ratio")
-
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier.fillMaxWidth()
-    ) {
-        // Bar
-        Box(
-            modifier = Modifier
-                .background(barColor, shape = RoundedCornerShape(8.dp))
-//                .width(maxBarWidth * (value / maxValue)) // 这里必须用 dp，用 float 的条形是一样的，全部充满，不会有区分
-                .weight(ratio)
-                .height(barHeight)
-                .clickable { onClick() },
-            contentAlignment = Alignment.CenterStart
-        ) {
+    Layout(
+        content = {
+            Box(
+                modifier = Modifier
+                    .background(barColor, shape = RoundedCornerShape(8.dp))
+                    .height(barHeight)
+                    .clickable { onClick() },
+                contentAlignment = Alignment.CenterStart
+            ) {
+                Text(
+                    text = durationText,
+                    color = Color.White,
+                    modifier = Modifier.padding(start = 8.dp)
+                )
+            }
             Text(
-//                text = value.toInt().toString(),
-                text = durationText,
-                color = Color.White,
-                modifier = Modifier.padding(start = 8.dp)
+                text = percent,
+                color = barColor,
+                fontWeight = FontWeight.SemiBold,
+                modifier = Modifier.padding(start = 10.dp)
             )
+        },
+        measurePolicy = { measurables, constraints ->
+            val barMeasurable = measurables[0]
+            val textMeasurable = measurables[1]
+
+            val textPlaceable = textMeasurable.measure(constraints)
+            val barWidth = constraints.maxWidth - textPlaceable.width
+            val barPlaceable = barMeasurable.measure(
+                Constraints.fixedWidth((barWidth * ratio).toInt())
+            )
+
+            val width = constraints.maxWidth
+            val height = maxOf(barPlaceable.height, textPlaceable.height)
+
+            layout(width, height) {
+                barPlaceable.place(0, 0)
+                textPlaceable.place(barPlaceable.width, (height - textPlaceable.height) / 2)
+            }
         }
-
-        // Spacer
-        Spacer(modifier = Modifier.width(8.dp))
-
-        // Percentage
-        Text(
-            text = percent,
-            color = barColor,
-            fontWeight = FontWeight.SemiBold,
-            modifier = if (ratio >= 1f) Modifier else Modifier.weight((1 - ratio)) // invalid weight 0.0; must be greater than zero
-        )
-    }
+    )
 }
